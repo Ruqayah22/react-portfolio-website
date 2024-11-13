7471a574e46d8c734e2289e554c3b0397d01fe35
ruqayah22 <ruqaya.alnawfaly@outlook.com>
2024-11-13 23:25:41 +0300

    finsh the whole project

--- README.md
+++ README.md
@@ -26,3 +26,50 @@
 ## for the testimonial slide
 - [slides](https://swiperjs.com/)
 - npm i swiper
+
+## send email from your contact form: 
+- [email.js](https://www.emailjs.com/)
+- Send Email Directly From Your Code
+  1. sign up.
+  2. add new services.
+  3. choose what service you want (in our code now choose Gmail).
+  4. you can name it what you want, live the service id as it 
+  5. click connect account, then choose the email you want to connect with it, click continue.
+  6. click create service.
+
+- then create email templates:
+  1. from the email templates in the sideMenu.
+  2. choose create new template.
+  3. we can write the what we want subject (like message from react portfolio). (if we put {{{subject}}} it will take the subject name from the form in the react project).
+  4. in the content area we can put the template to how the message will be when it received.
+  5. it will take it's content from the form in the project like this:
+  ```
+  {{{name}}}
+  {{{email}}}
+  {{{message}}}
+  ``` 
+  6. like the inputs fields in the react form.
+  7. click save button.
+  8. we can test it by clicking test it button.
+
+## react email js:
+- [react emailjs](https://www.emailjs.com/docs/examples/reactjs/)
+- npm install emailjs-com --save
+- import the code we need from the like above.
+``` emailjs
+      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", form.current , 'YOUR_USER_ID')
+``` 
+- we take is information the emailjs we created it.
+- YOUR_USER_ID -> emailjs.com -> integration -> API -> api keys -> user ID 
+- and from the contact form from the project test it.
+
+******
+## Deployment the code:
+- in the package.json file under the "name" this line code 
+` "homepage": "http://egatortutorials.com" // domain name`
+- npm run build
+- compress the build file to zip file.
+- log in or sign up to hostinger.com 
+   [hostinger](https://hpanel.hostinger.com)
+- the completed steps in this link:
+    [hostinger deployment](https://www.youtube.com/watch?v=G-Cr00UYokU&t=11930s)
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.css'
 import { MdOutlineEmail } from "react-icons/md";
 import { RiMessengerLine } from "react-icons/ri";
 import { BsWhatsapp } from "react-icons/bs";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", form.current , 'YOUR_USER_ID')
+      e.target.reset();
+  };
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -13,7 +23,7 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail />
+            <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>dummyegator@gmail.com</h5>
             <a
@@ -25,7 +35,7 @@ const Contact = () => {
             </a>
           </article>
           <article className="contact__option">
-            <RiMessengerLine />
+            <RiMessengerLine className="contact__option-icon" />
             <h4>Messenger</h4>
             <h5>egator tutorials</h5>
             <a href="https://m.me/facbookId" target="_blank" rel="noreferrer">
@@ -33,7 +43,7 @@ const Contact = () => {
             </a>
           </article>
           <article className="contact__option">
-            <BsWhatsapp />
+            <BsWhatsapp className="contact__option-icon" />
             <h4>WhatsApp</h4>
             <h5>+123456789</h5>
             <a
@@ -46,8 +56,23 @@ const Contact = () => {
           </article>
         </div>
         {/* end of contact options */}
-        <form action="">
-          
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
         </form>
       </div>
     </section>
